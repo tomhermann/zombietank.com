a600f853200980d4f3b6728ea3be423a74db089b
Tom Hermann <tomhermann@gmail.com>
2012-03-20 22:00:13 -0500

    Added gitignore, consolidated configuration

--- src/main/java/com/zombietank/config/SecurityConfig.java
+++
@@ -1,9 +0,0 @@
-package com.zombietank.config;
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.ImportResource;
-
-@Configuration
-@ImportResource("WEB-INF/spring/security-context.xml")
-public class SecurityConfig {
-}
