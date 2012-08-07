c6a592d543e2fdc5fcfba9574d359e6b8893efe6
Tom Hermann <tomhermann@gmail.com>
2012-08-07 18:45:57 -0500

    Moved logging to actual script execution

--- src/main/java/com/zombietank/config/db/DatabaseBootstrap.java
+++ src/main/java/com/zombietank/config/db/DatabaseBootstrap.java
@@ -1,11 +1,8 @@
 package com.zombietank.config.db;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.InitializingBean;
 
 public class DatabaseBootstrap implements InitializingBean {
-	private static final Logger logger = LoggerFactory.getLogger(DatabaseBootstrap.class);
 	private final ScriptRunner scriptRunner;
 	private final Scripts scripts;
 
@@ -16,9 +13,7 @@ public class DatabaseBootstrap implements InitializingBean {
 
 	public void executeScripts() {
 		for (Script script : scripts) {
-			logger.info("Executing {}", script);
 			scriptRunner.execute(script);
-			logger.info("Execution complete.");
 		}			
 	}
 	
--- src/main/java/com/zombietank/config/db/ScriptRunner.java
+++ src/main/java/com/zombietank/config/db/ScriptRunner.java
@@ -2,10 +2,13 @@ package com.zombietank.config.db;
 
 import javax.sql.DataSource;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 public class ScriptRunner {
+	private static final Logger logger = LoggerFactory.getLogger(ScriptRunner.class);
 	private final ScriptHistory scriptHistory;
 	private final JdbcTemplate jdbcTemplate;
 
@@ -17,10 +20,13 @@ public class ScriptRunner {
 	public void execute(Script script) throws DataAccessException {
 		if(script.isSystem() || scriptHistory.hasNotBeenRun(script)) {
 			run(script);
+		} else {
+			logger.info("Not running {}", script.getPath());
 		}
 	}
 	
 	private void run(Script script) {
+		logger.info("Executing script: {}", script);
 		jdbcTemplate.execute(script.getContents());
 		scriptHistory.recordRun(script);
 	}
