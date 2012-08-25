d28dec04e36b5c164462f8ee907a7cae2a9485ec
Tom Hermann <tomhermann@gmail.com>
2012-08-25 11:46:50 -0500

    Giving up on @Secured annotations until I have more time to mess with custom authentication classes

--- src/main/java/com/zombietank/contact/ContactMessageController.java
+++ src/main/java/com/zombietank/contact/ContactMessageController.java
@@ -4,14 +4,12 @@ import java.util.Collection;
 
 import javax.inject.Inject;
 
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
-@Secured("ROLE_SUPERVISOR")
 @RequestMapping("/contact-messages")
 public class ContactMessageController {
 	@Inject
