8832c30590dd6de5ea00e4078cd17ac8b3c40afd
Tom Hermann <tomhermann@gmail.com>
2012-08-25 11:53:15 -0500

    Added redirect for contact-messages get request

--- src/main/java/com/zombietank/contact/ContactMessageController.java
+++ src/main/java/com/zombietank/contact/ContactMessageController.java
@@ -7,6 +7,7 @@ import javax.inject.Inject;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
@@ -15,12 +16,17 @@ public class ContactMessageController {
 	@Inject
 	private ContactMessageRepository messageRepository;
 
+	@RequestMapping(method = RequestMethod.GET)
+	public String index() {
+		return "redirect:/all";
+	}
+
 	@RequestMapping("all")
 	@ResponseBody
 	public Collection<ContactMessage> showAllMessages() {
 		return messageRepository.getAllMessages();
 	}
-	
+
 	@RequestMapping("message/{id}")
 	@ResponseBody
 	public ContactMessage showMessage(@PathVariable long id) {
